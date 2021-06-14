5d5b7383d42f1953f98a141190afc174e2352dba
Monique <moniqueking57@gmail.com>
2021-06-14 11:49:19 +0100

    Servers running

---
+++ server/qadata.sql
@@ -0,0 +1,30 @@
+drop table if exists questions;
+drop table if exists answers;
+drop table if exists login;
+
+CREATE TABLE questions (
+  id          SERIAL PRIMARY KEY,
+  title       VARCHAR(50) NOT NULL,
+  body        VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE answers (
+  id          SERIAL PRIMARY KEY,
+  title       VARCHAR(50) NOT NULL,
+  body        VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE login (
+  id          SERIAL PRIMARY KEY,
+  first_name       VARCHAR(50) NOT NULL,
+  second_name        VARCHAR(50) NOT NULL,
+  email           VARCHAR(50) NOT NULL,
+);
+
+
+INSERT INTO questions (id, title, body) VALUES ('3','Never ', '24')
+
+INSERT INTO answers (id, title, body) VALUES ('3','Hi ', '25'),
+
+INSERT INTO login (id, first_name, second_name, email) VALUES ('1','Monique ', 'King', 'moniqueking57@gmail.com'),
+INSERT INTO login (id, first_name, second_name, email) VALUES ('2','Amanuel ', 'Tedros', 'amanueltedros@gmail.com'),
--- server/server.js
+++ server/server.js
@@ -3,6 +3,25 @@ import http from "http";
 import app from "./app";
 import { connectDb, disconnectDb } from "./db";
 
+// const express = require('express');
+// const {Pool} = require('pg');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+
+const dbConfig = {
+  host: 'localhost',
+  port: 5432,
+  user: 'moniqueking',
+  password: '',
+  database: 'qadata',
+};
+
+// const pool = new Pool(dbConfig);
+
 const port = parseInt(process.env.PORT || "3000");
 
 const server = http.createServer(app);
@@ -13,6 +32,17 @@ server.on("listening", () => {
 	// eslint-disable-next-line no-console
 	console.log(`Listening on ${bind}`);
 });
+
+app.get('/', (req, res) => {
+  res.send({express: 'Your Backend Service is Running'});
+});
+
+//Get all questions
+// Create new questions
+//create new answers
+//create new login info
+//delete questions and answers
+//get all open questions
 
 process.on("SIGTERM", () => server.close(() => disconnectDb()));
 
