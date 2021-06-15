d452e687130fbb784ae2b40c61dc89618bc8878e
Amanuel <weditedi25@gmail.com>
2021-06-15 14:50:44 +0100

    All routes linked and Some Css added

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom";
 
 const Login = () => (
-  <div>
+  <div className="loginContainer">
     <form className="loginForm">
       <h1>Log in</h1>
       <p>Log in to Ask a Question</p>
@@ -11,7 +11,9 @@ const Login = () => (
       <input type="submit" value="Log in" />
     </form>
     <p>Forgot your password?</p>
-		<p>Don't have an account?Sign up</p>
+    <p>
+      Don't have an account?<Link to="/SignUp">Sign Up</Link>
+    </p>
   </div>
 );
 
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,17 +1,20 @@
 import {Link} from "react-router-dom";
 
 const Login = () => (
-  <div>
-    <form className="loginForm">
+  <div className="signUpContainer">
+    <form className="signUpForm">
       <h1>Sign Up</h1>
       <p>
         Join the community to Ask and Answer questions from fellow developers
       </p>
       <input type="text" placeholder="Username" required />
       <input type="text" placeholder="Email" required />
-      <input type="password" placeholder="password" required />
+      <input type="Password" placeholder="password" required />
+      <input type="submit" value="Sign Up" />
     </form>
-    <p>Already have an account?Login</p>
+    <p>
+      Already have an account? <Link to="/login">Login</Link>
+    </p>
   </div>
 );
 
