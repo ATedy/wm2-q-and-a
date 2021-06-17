80b0254f32078c7ec9f511dd44b1d0d363378497
Monique <moniqueking57@gmail.com>
2021-06-17 21:57:07 +0100

    work

--- client/src/QuestionsForm.js
+++ client/src/QuestionsForm.js
@@ -1,24 +1,36 @@
 // import React from 'react';
 // import {forms} from 'react-bootstrap';
 
-// const NavBar = () => {
+// // const NavBar = () => {
 
-// <form>
+// // <form>
 //   <div class="form-group">
-//     <label for="exampleInputEmail1">Title</label>
-//     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-//     <small id="emailHelp" class="form-text text-muted">Be specific and imagine you're asking a question to another person.</small>
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Body</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-//   </div>
-//   <div class="form-group form-check">
-//     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-//     <label class="form-check-label" for="exampleCheck1">Tags</label>
-//   </div>
-//   <button type="submit" class="btn btn-primary">Ask</button>
-// </form>
+// //     <label for="exampleInputEmail1">Title</label>
+// //     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+// //     <small id="emailHelp" class="form-text text-muted">Be specific and imagine you're asking a question to another person.</small>
+// //   </div>
+// //   <div class="form-group">
+// //     <label for="exampleInputPassword1">Body</label>
+// //     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+// //   </div>
+// //   <div class="form-group form-check">
+// //     <input type="checkbox" class="form-check-input" id="exampleCheck1">
+// //     <label class="form-check-label" for="exampleCheck1">Tags</label>
+// //   </div>
+// //   <button type="submit" class="btn btn-primary">Ask</button>
+// // </form>
 
+// // }
+
+// class MyForm extends React.Component {
+//   render() {
+//     return (
+//       <form>
+//         <h1>Hello</h1>
+//         <p>Enter your name:</p>
+//         <input type="text" />
+//       </form>
+//     );
+//   }
 // }
-
+// ReactDOM.render(<MyForm />, document.getElementById('root'));
