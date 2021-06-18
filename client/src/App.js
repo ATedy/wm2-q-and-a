873250e33854ecb3057552252d5515156fb2d195
Amanuel <weditedi25@gmail.com>
2021-06-18 19:50:29 +0100

    Style changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,36 @@
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch, Link} from "react-router-dom";
 
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 
-
-const App = () => { 
-
-	return (
-	<Switch>
-		<Route path="/" exact><Home /></Route>
-		<Route path="/login"><Login /></Route>
-		<Route path="/SignUp"><SignUp />
-		</Route>
-	</Switch>
-);
-	}
+const App = () => {
+  return (
+    <main>
+      {/* <nav className="navBar">
+        <p>Quest</p>
+        <div>
+          <Link className="mr-3 navLinks" to="/SignUp">
+            Sign Up
+          </Link>
+          <Link className="navLinks" to="/login">
+            Login
+          </Link>
+        </div>
+      </nav> */}
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/SignUp">
+          <SignUp />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
 
 export default App;
