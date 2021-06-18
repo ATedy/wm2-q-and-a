08fd923b35171ded94b754a149e647c6001b1495
Monique <moniqueking57@gmail.com>
2021-06-18 16:09:17 +0100

    working on form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { Route, Switch } from "react-router-dom";
+ //import QuestionsForm from './QuestionsForm.js';
+
 
 import About from "./pages/About";
 import Home from "./pages/Home";
 
 const App = () => (
-	<Switch>
-		<Route path="/" exact><Home /></Route>
-		<Route path="/about/this/site"><About /></Route>
-	</Switch>
+  <Switch>
+    <Route path="/" exact>
+      <Home />
+    </Route>
+    <Route path="/about/this/site">
+      <About />
+    </Route>
+    {/* <QuestionsForm /> */}
+  </Switch>
 );
 
 export default App;
--- client/src/QuestionsForm.js
+++ client/src/QuestionsForm.js
@@ -1,36 +1,38 @@
-// import React from 'react';
-// import {forms} from 'react-bootstrap';
+import React from 'react';
+//import {forms} from 'react-bootstrap';
+import {
+  Container,
+  CardDeck,
+  Row,
+  Col,
+  Form,
+  InputGroup,
+  Dropdown,
+  FormGroup,
+  ToggleButtonGroup,
+} from 'react-bootstrap';
 
-// // const NavBar = () => {
+const QuestionsForm = () => {
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
 
-// // <form>
-//   <div class="form-group">
-// //     <label for="exampleInputEmail1">Title</label>
-// //     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-// //     <small id="emailHelp" class="form-text text-muted">Be specific and imagine you're asking a question to another person.</small>
-// //   </div>
-// //   <div class="form-group">
-// //     <label for="exampleInputPassword1">Body</label>
-// //     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-// //   </div>
-// //   <div class="form-group form-check">
-// //     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-// //     <label class="form-check-label" for="exampleCheck1">Tags</label>
-// //   </div>
-// //   <button type="submit" class="btn btn-primary">Ask</button>
-// // </form>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>;
+}
 
-// // }
-
-// class MyForm extends React.Component {
-//   render() {
-//     return (
-//       <form>
-//         <h1>Hello</h1>
-//         <p>Enter your name:</p>
-//         <input type="text" />
-//       </form>
-//     );
-//   }
-// }
-// ReactDOM.render(<MyForm />, document.getElementById('root'));
+export default QuestionsForm;
