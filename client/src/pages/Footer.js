dadf9ae3848a6d8b5557b4c0fcfd2d4b6bdf69c8
mame-baba <mamebabambaye@gmail.com>
2021-07-02 20:07:11 +0100

    more added styling

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <footer className="list-footer">
+      <ul className="list">
+        <li>
+          <a href="https://www.facebook.com/codeyourfuture.io/" target="_blank">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/codeyourfuture_/?hl=en"
+            target="_blank"
+          >
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/CodeYourFuture?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+            target="_blank"
+          >
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a href="https://codeyourfuture.io/" target="_blank">
+            website
+          </a>
+        </li>
+      </ul>
+      <p className="copyright">Copyright &copy; 2021 CYF All Rights Reserved</p>
+    </footer>
+  );
+};
+export default Footer;
--- client/src/pages/NavBar.js
+++ client/src/pages/NavBar.js
@@ -1,35 +1,22 @@
-import React from 'react';
+import React from "react";
 // import Css from './app.css'
 // import './NavBar.css';
-import {Navbar, Container} from 'react-bootstrap';
+import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
   return (
-  
-   <nav className="navBar">
-       {/* <img
-      className="img-logo"
-      src="https://images-platform.99static.com/O1gSHEEF4AvFdCYpaeSzFp7iYY0=/500x500/top/smart/99designs-contests-attachments/26/26007/attachment_26007734"
-      alt="logo-image"
-    /> */}
-      <div>
-        <div>
-          <input
-            type="text"
-            placeholder="Search.."
-            className="prompt"
-          />
-          <button type="submit">
-          
-            Submit
-          </button>
+    <Navbar className="navBar">
+      <div className="search">
+        <p>Q U E S T</p>
+        <div className="search-bar">
+          <input type="text" placeholder="Search.." className="prompt" />
+          <button type="submit">Enter</button>
           <i className="search icon"></i>
         </div>
       </div>
-      <p>Q U E S T</p>
-      <div>
+
+      <div className="login">
         <Link className="mr-3 navLinks" to="/SignUp">
           Sign Up
         </Link>
@@ -37,11 +24,8 @@ const NavBar = () => {
           Login
         </Link>
       </div>
-    </nav>
-
+    </Navbar>
   );
-}
+};
 
 export default NavBar;
-
-  
