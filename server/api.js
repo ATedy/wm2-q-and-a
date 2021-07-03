c4b99e419330a4c44eac6e8434d0896b73e5d8b7
mame-baba <mamebabambaye@gmail.com>
2021-07-03 15:50:07 +0100

    changes to the form

--- server/api.js
+++ server/api.js
@@ -4,19 +4,15 @@ const router = new Router();
 const db = require("./db");
 // const customerSelectQuery = `select * from questions`;
 router.get("/", (_, res) => {
-	// res.json({ message: "Hello, world!" });
+  // res.json({ message: "Hello, world!" });
 
-	pool
-	.query('SELECT * FROM questions;')
-	.then((result) =>{
-		// console.log(result.rows);
-		res.send(result.rows)})
-	.catch((err) => console.log(err));
-	
+  pool
+    .query("SELECT * FROM questions;")
+    .then((result) => {
+      // console.log(result.rows);
+      res.send(result.rows);
+    })
+    .catch((err) => console.log(err));
 });
-
-
-
-
 
 export default router;
