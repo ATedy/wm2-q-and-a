3928d9aa27be17e7b94d5a7b98aab232ebe09e87
Amanuel <weditedi25@gmail.com>
2021-07-09 16:13:02 +0100

    search updated

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import {useEffect, useState, useContext} from 'react';
+import React from "react";
+import {useEffect, useState, useContext} from "react";
 import {SearchResultContext} from "../context/SearchResultContext";
 
-
 const Search = () => {
-    const {questions, answers, setQuestions, setAnswers, searchString, setSearchString} = useContext(SearchResultContext);
-    const handleChange = (event) => {
+  const {
+    questions,
+    answers,
+    setQuestions,
+    setAnswers,
+    searchString,
+    setSearchString,
+  } = useContext(SearchResultContext);
+  const handleChange = (event) => setSearchString(event.target.value);
 
-         setSearchString(event.target.value)
-     }
-console.log({questions});
-console.log({answers});
-return(
-        <input  
-          id="livesearch"
-          type="text"
-          placeholder="Search.."
-          className="prompt" 
-          onChange={handleChange} 
-        />
-  )
-}
+  console.log({questions});
+  console.log({answers});
+  return (
+    <input
+      id="livesearch"
+      type="text"
+      placeholder="Search.."
+      className="prompt"
+      onChange={handleChange}
+    />
+  );
+};
 
-export default Search
- 
- 
+export default Search;
